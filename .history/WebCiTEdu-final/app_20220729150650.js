96d6ce67a0ba90764df6cbd8f5eee7bdd6febf6b
Daivu2905 <101865346+Daivu2905@users.noreply.github.com>
2022-07-29 16:16:14 +0700

    sua

--- .history/WebCiTEdu-final/app_20220729150650.js
+++ .history/WebCiTEdu-final/app_20220729150650.js
@@ -1,4 +1,45 @@
 $(document).ready(function() {
+
+    $(function() {
+        // on init
+        $(".table-hideable .hide-col").each(HideColumnIndex);
+
+        // on click
+        $('.hide-column').click(HideColumnIndex)
+
+        function HideColumnIndex() {
+            var $el = $(this);
+            var $cell = $el.closest('th,td')
+            var $table = $cell.closest('table')
+
+            // get cell location - https://stackoverflow.com/a/4999018/1366033
+            var colIndex = $cell[0].cellIndex + 1;
+            // find and hide col index
+            $table.find("tbody tr, thead tr")
+                .children(":nth-child(" + colIndex + ")")
+                .addClass('hide-col')
+
+            // show restore footer
+            $table.find(".footer-restore-columns").show()
+        }
+
+        // restore columns footer
+        $(".restore-columns").click(function(e) {
+            var $table = $(this).closest('table')
+            $table.find(".footer-restore-columns").hide()
+            $table.find("th, td")
+                .removeClass('hide-col');
+
+        })
+
+        $('[data-toggle="tooltip"]').tooltip({
+            trigger: 'hover'
+        })
+
+    })
+
+
+
     // on init
     $(".table-hideable .hide-col").each(HideColumnIndex);
 
@@ -38,7 +79,7 @@ $(document).ready(function() {
     $("#insertCol").click(function() {
         let html1 = "";
 
-        html1 += `<th>Buổi ${stt} <button style="border: 0; background-color: #F8F9FC" id="select-all" class="checkBox${stt}"><i
+        html1 += `<th id = ' + stt + '></th>Buổi ${stt} <button style="border: 0; background-color: #F8F9FC" id="select-all" class="checkBox${stt}"><i
   class="fa-solid fa-check-double" style="color:green;"  data-toggle="tooltip" data-placement="right" title="Chọn tất cả"></i></button><br><button type="button"
 class="btn" data-toggle="modal" data-target="#exampleModal">
 Thêm
@@ -95,9 +136,9 @@ Thêm
     });
     $('#select-all1').click(function() {
         if ($(this).hasClass('allChecked')) {
-            $('input:checkbox[id ="row${i + 2}`"][type="checkbox"]').prop('checked', false);
+            $('input:checkbox[id^=' + stt + '][type="checkbox"]').prop('checked', false);
         } else {
-            $('input:checkbox[id^="row${i + 2}`"][type="checkbox"]').prop('checked', true);
+            $('input:checkbox[id^=' + stt + '][type="checkbox"]').prop('checked', true);
         }
         $(this).toggleClass('allChecked');
     })
--- .history/WebCiTEdu-final/app_20220729150841.js
+++ .history/WebCiTEdu-final/app_20220729150841.js
@@ -1,4 +1,45 @@
 $(document).ready(function() {
+
+    $(function() {
+        // on init
+        $(".table-hideable .hide-col").each(HideColumnIndex);
+
+        // on click
+        $('.hide-column').click(HideColumnIndex)
+
+        function HideColumnIndex() {
+            var $el = $(this);
+            var $cell = $el.closest('th,td')
+            var $table = $cell.closest('table')
+
+            // get cell location - https://stackoverflow.com/a/4999018/1366033
+            var colIndex = $cell[0].cellIndex + 1;
+            // find and hide col index
+            $table.find("tbody tr, thead tr")
+                .children(":nth-child(" + colIndex + ")")
+                .addClass('hide-col')
+
+            // show restore footer
+            $table.find(".footer-restore-columns").show()
+        }
+
+        // restore columns footer
+        $(".restore-columns").click(function(e) {
+            var $table = $(this).closest('table')
+            $table.find(".footer-restore-columns").hide()
+            $table.find("th, td")
+                .removeClass('hide-col');
+
+        })
+
+        $('[data-toggle="tooltip"]').tooltip({
+            trigger: 'hover'
+        })
+
+    })
+
+
+
     // on init
     $(".table-hideable .hide-col").each(HideColumnIndex);
 
@@ -38,13 +79,13 @@ $(document).ready(function() {
     $("#insertCol").click(function() {
         let html1 = "";
 
-        html1 += `<th>Buổi ${stt} <button style="border: 0; background-color: #F8F9FC" id="select-all" class="checkBox${stt}"><i
+        html1 += `<thz>Buổi ${stt} <button style="border: 0; background-color: #F8F9FC" id="select-all" class="checkBox${stt}"><i
   class="fa-solid fa-check-double" style="color:green;"  data-toggle="tooltip" data-placement="right" title="Chọn tất cả"></i></button><br><button type="button"
 class="btn" data-toggle="modal" data-target="#exampleModal">
 Thêm
 </button>  <button class="pull-right btn btn-default btn-condensed hide-column" data-toggle="tooltip" data-placement="bottom" title="Hide Column">
 <i class="fa fa-eye-slash"></i>  
-</button> </th>`;
+</button> </thz>`;
         $("#row1").append(html1);
         for (let i = 0; i < 5; i++) {
 
